dbd0043be0c3c5b8bd60a450e13f6292117ea3a8
rohit elamurugan <rohitelamurugan1607@gmail.com>
2023-03-22 22:28:44 +0530

    changes

--- linked-list.py
+++ linked-list.py
@@ -12,6 +12,7 @@ class LinkedList:
         while item is not None:
             print(item.val)
             item = item.next
+    
 
 llist = LinkedList()
 llist.head = Node(1)
@@ -19,5 +20,4 @@ e2 = Node(2)
 e3 = Node(3)
 llist.head.next = e2
 e2.next = e3
-
 llist.PrintList()
