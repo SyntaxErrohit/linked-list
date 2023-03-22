8d05617aa5d1cc10c51535d8807c0999fb4ea940
rohit elamurugan <rohitelamurugan1607@gmail.com>
2023-03-22 22:17:52 +0530

    traversed the list

---
+++ linked-list.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, val=None) -> None:
+        self.val = val
+        self.next = None
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+
+    def PrintList(self) -> None:
+        item = self.head
+        while item is not None:
+            print(item.val)
+            item = item.next
+
+llist = LinkedList()
+llist.head = Node(1)
+e2 = Node(2)
+e3 = Node(3)
+llist.head.next = e2
+e2.next = e3
+
+llist.PrintList()
